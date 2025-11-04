3d5e65c6756a0154bed73edb98697686807cd1ea
gimgyuwon <gimgyuwon2@gmail.com>
2025-11-04 13:55:59 +0900

    feat: Implement header component and import logo font

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,8 @@
+import Header from "./Header";
+
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <div className="bg-bg w-full max-w-[420px] min-h-screen mx-auto flex flex-col items-center justify-center">
+  <div className="bg-bg w-full max-w-[420px] min-h-screen mx-auto flex flex-col items-center">
+    <Header />
     {children}
   </div>
 );
