9c33aba88005fc8db165ed04911e4654023c54ed
gimgyuwon <gimgyuwon2@gmail.com>
2025-11-04 14:15:29 +0900

    feat: Implement footer component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="w-full flex flex-row justify-between h-[44px] px-5 py-2">
+    <div className="w-full flex flex-row justify-between h-[44px] px-[20px] py-[10px]">
       {/* logo */}
       <div className="font-aggro text-[#FE7171] font-black text-xl">
         이심전심
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,11 @@
+import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className="bg-bg w-full max-w-[420px] min-h-screen mx-auto flex flex-col items-center">
     <Header />
-    {children}
+    <div className="flex flex-1">{children}</div>
+    <Footer />
   </div>
 );
 
