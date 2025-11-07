a617dc9005f5e7d2d6f3011f6ad3be5eea623c1d
hardwoong <dew04300@naver.com>
2025-11-08 01:44:07 +0900

    feat: 구현할 페이지들 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,33 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Layout from "./components/layout/Layout";
+import Splash from "./pages/Splash";
+import Onboard from "./pages/Onboard";
+import OnboardJoin from "./pages/OnboardJoin";
+import OnboardJoinConfirm from "./pages/OnboardJoinConfirm";
+import OnboardProfile from "./pages/OnboardProfile";
+import OnboardCreate from "./pages/OnboardCreate";
+import OnboardCreateComplete from "./pages/OnboardCreateComplete";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
+          {/* splash */}
+          <Route path="/" element={<Splash />} />
           {/* login */}
-          <Route index element={<Login />} />
+          <Route path="/login" element={<Login />} />
           {/* onboard */}
-
+          <Route path="/onboard" element={<Onboard />} />
+          <Route path="/onboard/join" element={<OnboardJoin />} />
+          <Route path="/onboard/join/confirm" element={<OnboardJoinConfirm />} />
+          <Route path="/onboard/create" element={<OnboardCreate />} />
+          <Route
+            path="/onboard/create/complete"
+            element={<OnboardCreateComplete />}
+          />
+          <Route path="/onboard/profile" element={<OnboardProfile />} />
           {/* home */}
           <Route path="/home" element={<Home />} />
 
